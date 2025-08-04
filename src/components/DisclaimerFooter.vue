be1a07e10b9cb0d5845293e68b6964228f4943fa
ahpakar <apakar@miraculind.eu>
2025-08-04 17:01:42 +0300

    Clarification in footer

--- src/components/DisclaimerFooter.vue
+++ src/components/DisclaimerFooter.vue
@@ -8,6 +8,9 @@
         <div class="text-centre">
             <p>Event contracts are not suitable for all investors.  When investing, your capital is at risk and you may get back less than invested.  Past performance doesn't guarantee future results.  Retail investors may lose significant sums of money when investing in complex instruments such as event contracts.</p>
         </div>
+        <div class="text-centre">
+            <p>This is a fictional market that has no relation to real-life securities exchanges or brokerages.  We are not registered in any real-world country, nor do we deal in real-world money.</p>
+        </div>
         <div class="text-centre"><b>Miraculin–Daemon Markets Ltd.</b></div>
     </footer>
 </template>
