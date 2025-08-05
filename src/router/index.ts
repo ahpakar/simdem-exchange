57863a0b4d8618a4dfbfb616de619d3f298ffd2b
ahpakar <apakar@miraculind.eu>
2025-08-05 16:21:15 +0300

    chore: router cleanup

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,40 +6,19 @@ import UnderwritingPage from '../components/UnderwritingPage.vue'
 import WarningsDisclaimers from '../components/WarningsDisclaimers.vue'
 import TermsConditions from '../components/TermsConditions.vue'
 
+const routes: any[] = [
+    {path: '/', name: 'home', component: LearnPage},
+    {path: '/learn', redirect: '/'},
+    {path: '/market', name: 'market', component: MarketPage},
+    {path: '/underwriting', name: 'underwriting', component: UnderwritingPage},
+    {path: '/legal', name: 'legal', component: WarningsDisclaimers},
+    {path: '/terms-conditions', name: 'terms-conditions', component: TermsConditions},
+    {path: '/privacy', name: 'privacy', component: Privacy},
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: LearnPage,
-    },
-    {
-        path: '/market',
-        name: 'market',
-        component: MarketPage,
-    },
-    {
-        path: '/underwriting',
-        name: 'underwriting',
-        component: UnderwritingPage,
-    },
-    {
-        path: '/legal',
-        name: 'legal',
-        component: WarningsDisclaimers,
-    },
-    {
-        path: '/terms-conditions',
-        name: 'terms-conditions',
-        component: TermsConditions,
-    },
-    {
-        path: '/privacy',
-        name: 'privacy',
-        component: Privacy,
-    },
-  ],
-})
+  routes,
+});
 
-export default router
+export default router;
